b028df70dcf3a96edea6fee5c7766633e7db9e77
Ayesha Shaw <ayeshashaw520@gmail.com>
2025-04-04 19:43:42 +0530

    Update AdminContext.jsx

--- admin-panel/src/context/AdminContext.jsx
+++ admin-panel/src/context/AdminContext.jsx
@@ -10,7 +10,7 @@ const AdminContextProvider = (props) => {
   const[appointments,setAppointments] = useState([])
   const[dashData,setDashData] = useState(false)
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://docmeet-g0lg.onrender.com";
 
   const getAllDoctors = async () => {
     try {
